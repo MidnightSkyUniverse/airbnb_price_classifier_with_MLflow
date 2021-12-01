9567a545d935ba6a4e1c51ee633c2aca67dbb3f2
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-12-01 19:55:16 +0100

    update to basic_cleaning to drop entries outside of NY

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     logging.info('Change format of last_review column from string to date')
     df['last_review'] = pd.to_datetime(df['last_review'])    
 
+    # Remove entries out of NY location
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     filename = args.output_artifact
     df.to_csv(filename, index=False)
 
